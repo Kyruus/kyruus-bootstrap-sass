91ed6012769015357988a0af7db3276b45d3e6b5
Travis Chase <travis@travischase.me>
2013-07-24 10:07:18 -0500

    More update fixes

--- lib/kyruus/bootstrap/sass/engine.rb
+++ lib/kyruus/bootstrap/sass/engine.rb
@@ -3,7 +3,7 @@ module Kyruus
     module Sass
       class Engine < ::Rails::Engine
         initializer "kyruus-bootstrap-sass.assets.precompile" do |app|
-          app.config.assets.precompile += %w(*.otf *.eot *.svg *.ttf *.woff)
+          app.config.assets.precompile += %w(*.otf *.eot *.svg *.ttf *.woff *.gif *.jpg)
         end
       end
     end
--- lib/kyruus/bootstrap/sass/version.rb
+++ lib/kyruus/bootstrap/sass/version.rb
@@ -1,7 +1,7 @@
 module Kyruus
   module Bootstrap
     module Sass
-      VERSION = "1.0.0.1"
+      VERSION = "1.0.0.3"
     end
   end
 end
