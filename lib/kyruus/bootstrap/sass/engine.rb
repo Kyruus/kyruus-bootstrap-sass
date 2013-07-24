3ce9f2bd8d3a4f2219d1d00b822e74bca86ae167
Travis Chase <travis@travischase.me>
2013-07-24 09:06:45 -0500

    Fixes to get the sass working in Rails app

--- lib/kyruus/bootstrap/sass/engine.rb
+++ lib/kyruus/bootstrap/sass/engine.rb
@@ -2,6 +2,9 @@ module Kyruus
   module Bootstrap
     module Sass
       class Engine < ::Rails::Engine
+        initializer "kyruus-bootstrap-sass.assets.precompile" do |app|
+          app.config.assets.precompile += %w(*.otf *.eot *.svg *.ttf *.woff)
+        end
       end
     end
   end
--- lib/kyruus/bootstrap/sass/version.rb
+++ lib/kyruus/bootstrap/sass/version.rb
@@ -1,7 +1,7 @@
 module Kyruus
   module Bootstrap
     module Sass
-      VERSION = "1.0.0"
+      VERSION = "1.0.0.1"
     end
   end
 end
